3101066d524798edb048035645207bcbae980f39
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-09 17:08:38 +0300

    Да се напише програма, която чете n-на брой цели числа, въведени от потребителя,и проверява дали сред тях съществува число, което е равно на сумата на всички останали. •	Ако има такъв елемент печата "Yes" и на нов ред "Sum = "  + неговата стойност •	Ако няма такъв елемент печата "No" и на нов ред "Diff = " + разликата между най-големия елемент и сумата на останалите (по абсолютна стойност)

---
+++ For-Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ForLoopSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a, sum = 0, max = 0;
+            for (int i = 0; i < n; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+                sum += a;
+                if (max < a)
+                {
+                    max = a;
+                }
+            }
+            int withoutMax = sum - max;
+            if (max == withoutMax)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {max}");
+            }
+            else
+            {
+                withoutMax -= max;
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(withoutMax)}");
+            }
+        }
+    }
+}
