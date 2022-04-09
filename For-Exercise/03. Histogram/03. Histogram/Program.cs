c37d86da7da2798ca8fcfeaa47fb71720c5d2c14
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-09 17:10:47 +0300

    Дадени са n цели числа в интервала [1…1000]. От тях някакъв процент p1 са под 200, друг процент p2 са от 200 до 399, друг процент p3 са от 400 до 599, друг процент p4 са от 600 до 799 и останалите p5 процента са от 800 нагоре. Да се напише програма, която изчислява и отпечатва процентите p1, p2, p3, p4 и p5.

---
+++ For-Exercise/03. Histogram/03. Histogram/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ForLoopSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a;
+            double p1 = 0, p2 = 0, p3 = 0, p4 = 0, p5 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+                if (a < 200)
+                {
+                    p1++;
+                }
+                else if (a >= 200 && a < 400)
+                {
+                    p2++;
+                }
+                else if (a >= 400 && a < 600)
+                {
+                    p3++;
+                }
+                else if (a >= 600 && a < 800)
+                {
+                    p4++;
+                }
+                else if (a >= 800 && a <= 1000)
+                {
+                    p5++;
+                }
+            }
+            double procent = 100 / Convert.ToDouble(n);
+            p1 *= procent;
+            p2 *= procent;
+            p3 *= procent;
+            p4 *= procent;
+            p5 *= procent;
+            Console.WriteLine($"{p1:f2}%");
+            Console.WriteLine($"{p2:f2}%");
+            Console.WriteLine($"{p3:f2}%");
+            Console.WriteLine($"{p4:f2}%");
+            Console.WriteLine($"{p5:f2}%");
+        }
+    }
+}
