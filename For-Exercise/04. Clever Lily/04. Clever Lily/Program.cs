d22df35f3adad5dbc1b51f11857572c651f7e7aa
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-11 09:43:44 +0300

    Лили вече е на N години. За всеки свой рожден ден тя получава подарък. •	За нечетните рождени дни (1, 3, 5...n) получава играчки. •	За четните рождени дни (2, 4, 6...n) получава пари. За втория рожден ден получава 10.00 лв, като сумата се увеличава с 10.00 лв., за всеки следващ четен рожден ден (2 -> 10, 4 -> 20, 6 -> 30...и т.н.). През годините Лили тайно е спестявала парите. Братът на Лили, в годините, които тя получава пари, взима по 1.00 лев от тях. Лили продала играчките получени през годините, всяка за P лева и добавила сумата към спестените пари. С парите искала да си купи пералня за X лева. Напишете програма, която да пресмята, колко пари е събрала и дали ѝ стигат да си купи пералня.

---
+++ For-Exercise/04. Clever Lily/04. Clever Lily/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ForLoopSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double wash = double.Parse(Console.ReadLine());
+            int toy = int.Parse(Console.ReadLine());
+            int numberToy = 0, brother = 0, cashMultiplayer = 1;
+            double money = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    money += 10 * cashMultiplayer;
+                    cashMultiplayer++;
+                    brother++;
+                }
+                else
+                {
+                    numberToy++;
+                }
+            }
+            money += (numberToy * toy) - brother;
+            if (money >= wash)
+            {
+                Console.WriteLine($"Yes! {(money - wash):f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {(wash - money):f2}");
+            }
+        }
+    }
+}
