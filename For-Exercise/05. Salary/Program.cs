f95e361461e05763b0f417076a00b08a05efbdc4
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-12 13:30:18 +0300

    Шеф на компания забелязва че все повече служители прекарват  време в сайтове, които ги разсейват. За да предотврати това, той въвежда изненадващи проверки на отворените табове на браузъра на служителите си. Според отворения сайт в таба се налагат следните глоби: •	"Facebook" -> 150 лв. •	"Instagram" -> 100 лв. •	"Reddit" -> 50 лв. От конзолата се четат два реда: •	Брой отворени табове в браузъра n - цяло число в интервала [1...10] •	Заплата - число в интервала [500...1500] След това n – на брой пъти се чете име на уебсайт – текст

---
+++ For-Exercise/05. Salary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ForLoopSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double money = double.Parse(Console.ReadLine());
+            string site;
+            for (int i = 0; i < n; i++)
+            {
+                site = Console.ReadLine();
+                switch (site)
+                {
+                    case "Facebook":
+                        money -= 150;
+                        break;
+                    case "Instagram":
+                        money -= 100;
+                        break;
+                    case "Reddit":
+                        money -= 50;
+                        break;
+                }
+            }
+            if (money <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(Convert.ToInt32(money));
+            }
+        }
+    }
+}
