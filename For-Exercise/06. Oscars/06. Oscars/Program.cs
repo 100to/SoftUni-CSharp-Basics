db8ba5ee8186167105a4b01ba28eae8ae4b4c745
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-13 20:21:00 +0300

    Поканени сте от академията да напишете софтуер, който да пресмята точките за актьор/актриса. Академията ще ви даде първоначални точки за актьора. След това всеки оценяващ ще дава своята оценка. Точките, които актьора получава се формират от: дължината на името на оценяващия умножено по точките, които дава делено на две. Ако резултатът в някой момент надхвърли 1250.5 програмата трябва да прекъсне и да се отпечата, че дадения актьор е получил номинация.

---
+++ For-Exercise/06. Oscars/06. Oscars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ForLoopSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double points = double.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string jury;
+            double juryPoints;
+            for (int i = 0; i < n; i++)
+            {
+                jury = Console.ReadLine();
+                juryPoints = double.Parse(Console.ReadLine());
+                points += Math.Round(((juryPoints * jury.Length) / 2), 2, MidpointRounding.AwayFromZero);
+                if (points > 1250.5)
+                {
+                    i = n;
+                }
+            }
+            if (points > 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {name} got a nominee for leading role with {points:f1}!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, {name} you need {1250.5 - points:f1} more!");
+            }
+        }
+    }
+}
