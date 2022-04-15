3b55fd7adbe30155e5b70e9e43d1b77e190843ca
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-15 21:26:51 +0300

    Григор Димитров е тенисист, чиято следваща цел е изкачването в световната ранглиста по тенис за мъже. През годината Гришо участва в определен брой турнири, като за всеки турнир получава точки, които зависят от позицията, на която е завършил в турнира. Има три варианта за завършване на турнир: 	W - ако е победител получава 2000 точки 	F - ако е финалист получава 1200 точки 	SF - ако е полуфиналист получава 720 точки Напишете програма, която изчислява колко ще са точките на Григор след изиграване на всички турнири, като знаете с колко точки стартира сезона. Също изчислете колко точки средно печели от всички изиграни турнири и колко процента от турнирите е спечелил.

---
+++ For-Exercise/08. Tennis Ranklist/08. Tennis Ranklist/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ForLoopSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double newPoints = double.Parse(Console.ReadLine());
+            double points = 0, win = 0;
+            string game;
+            for (int i = 0; i < n; i++)
+            {
+                game = Console.ReadLine();
+                switch (game)
+                {
+                    case "W":
+                        {
+                            points += 2000;
+                            win++;
+                            break;
+                        }
+                    case "F":
+                        points += 1200;
+                        break;
+                    case "SF":
+                        points += 720;
+                        break;
+                }
+            }
+            points += newPoints;
+            double percent = 100 / Convert.ToDouble(n);
+            Console.WriteLine($"Final points: {points}");
+            Console.WriteLine($"Average points: {Math.Round((Convert.ToDouble(points - newPoints)) / n, MidpointRounding.ToNegativeInfinity)}");
+            Console.WriteLine($"{Math.Round(win * percent, 2, MidpointRounding.AwayFromZero):f2}%");
+
+        }
+    }
+}
